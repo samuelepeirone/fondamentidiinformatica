d35a0cd3c551bf0b547d1452f1c7fa3fed69377c
samuelepeirone <samuelepeirone2001@gmail.com>
2023-01-26 14:01:49 +0100

    committomag

--- calcolo_recommender.php
+++ calcolo_recommender.php
@@ -15,8 +15,23 @@
 </head>
 <body>
     <?php
-        stampacardfilm();
+        $conn = connetti("film_recomender");
+        $s = "SELECT id_film FROM mytable";
+        $query = mysqli_query($conn, $s);
+        $n = mysqli_num_rows($query);
+        $s2 = "INSERT INTO utente_rate VALUES (88, ";
+        for($i=0; $i<$n; $i++){
+            $s2 = $s2.$_POST[$i+1];
+            //$film[$i] = $_POST[$i+1];
+            if($i<$n-1){
+                $s2 = $s2.", ";
+            }
+        }
+        $s2 = $s2.");";
+        $query2 = mysqli_query($conn, $s2);
     ?>
+    <h1>Ecco i film che abbiamo scelto per te: </h1>
+    
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"
         integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script src="codice.js"></script>
